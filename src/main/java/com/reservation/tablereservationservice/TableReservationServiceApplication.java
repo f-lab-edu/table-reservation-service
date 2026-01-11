f3fcf49fa3eedaa2a1ce3ef0a21341038992fd5d
jeondui <jeondui@gmail.com>
2026-01-11 18:31:27 +0900

    프로젝트 생성

---
+++ src/main/java/com/reservation/tablereservationservice/TableReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.reservation.tablereservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableReservationServiceApplication.class, args);
+	}
+
+}
