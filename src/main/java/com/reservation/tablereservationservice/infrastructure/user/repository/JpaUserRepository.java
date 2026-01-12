4b5a94d7dbc32b8dcd91fd762d1efd55aec044ee
jeondui <jeondui@gmail.com>
2026-01-12 14:26:36 +0900

    refactor: 도메인 UserRepository와 JPA Repository 분리

--- src/main/java/com/reservation/tablereservationservice/infrastructure/user/repository/JpaUserRepository.java
+++ src/main/java/com/reservation/tablereservationservice/infrastructure/user/repository/JpaUserRepository.java
@@ -1,8 +1,33 @@
 package com.reservation.tablereservationservice.infrastructure.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.reservation.tablereservationservice.domain.user.User;
+import com.reservation.tablereservationservice.domain.user.UserRepository;
 import com.reservation.tablereservationservice.infrastructure.user.entity.UserEntity;
 
-public interface JpaUserRepository extends JpaRepository<UserEntity, Long> {
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class JpaUserRepository implements UserRepository {
+
+	private final UserEntityRepository userEntityRepository;
+
+	@Override
+	public User save(User user) {
+
+		// UserEntity userEntity = UserEntity.from(user);
+		// UserEntity savedUserEntity =  userEntityRepository.save(userEntity);
+		//
+ 		// infra 레이어에서 도메인 -> 엔티티, 엔티티 -> 도메인 바꾸는 과정 필요
+		// return savedUserEntity.toDomain();
+
+		return null;
+	}
+
+	interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
+
+	}
 }
