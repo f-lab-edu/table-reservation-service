5f848410d27be07a8b9c53d804c9db4fb61b3e53
jeondui <jeondui@gmail.com>
2026-01-22 02:24:39 +0900

    feat: 예약 관련 오류코드 추가

--- src/main/java/com/reservation/tablereservationservice/global/exception/ErrorCode.java
+++ src/main/java/com/reservation/tablereservationservice/global/exception/ErrorCode.java
@@ -12,6 +12,9 @@ public enum ErrorCode {
 	// 400 Bad Request
 	INVALID_INPUT_VALUE("잘못된 입력 값입니다.", HttpStatus.BAD_REQUEST),
 	MISSING_PARAMETER("필수 파라미터가 누락되었습니다.", HttpStatus.BAD_REQUEST),
+	INVALID_PARTY_SIZE("예약 인원 수가 올바르지 않습니다.", HttpStatus.BAD_REQUEST),
+	RESERVATION_SLOT_NOT_OPENED("해당 날짜의 예약이 오픈되지 않았습니다.", HttpStatus.BAD_REQUEST),
+	INVALID_RESERVATION_REQUEST("유효하지 않은 예약 요청입니다.", HttpStatus.BAD_REQUEST),
 
 	// 401 Unauthorized
 	UNAUTHORIZED("인증되지 않은 사용자입니다.", HttpStatus.UNAUTHORIZED),
@@ -27,6 +30,11 @@ public enum ErrorCode {
 
 	// 409 Conflict
 	DUPLICATE_RESOURCE("%s 값이 이미 존재합니다.", HttpStatus.CONFLICT),
+	// 예약 정책 충돌
+	RESERVATION_DUPLICATED_TIME("동시간대에 이미 예약이 존재합니다.", HttpStatus.CONFLICT),
+	RESERVATION_CAPACITY_NOT_ENOUGH("예약 가능 좌석이 부족합니다.", HttpStatus.CONFLICT),
+	// 동시성 충돌
+	RESERVATION_NOT_AVAILABLE("다른 예약이 선점되었습니다.", HttpStatus.CONFLICT),
 
 	// 500 Internal Server Error
 	INTERNAL_SERVER_ERROR("서버 내부 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
